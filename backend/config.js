5f4f23087668bd655aabea89b1ac58d33e676080
Rajesh <94946346+RAJESH2132@users.noreply.github.com>
2024-07-18 12:29:47 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURl =
-  "mongodb+srv://rajesh_root:rajesh_root@book-store-mern.ugsthur.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Book-Store-Mern";
+  "Your-URL";
